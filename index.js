c64a98990977ca90cbbcb7ba3e84feb7642842ee
vazkido <vazkid02@gmail.com>
2025-02-20 10:03:38 -0500

    server update

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-
 });
 
 app.get("/val", (req, res) => {
-
+  res.send("BE CAREFUL MY LOVE!");
 });
+
+app.get("/val/:name", (req, res) => {
+ res.send(`<h1> KIND MAN! ${req.params.name} </h1>`);
+});
+
+app.get("*", (req, res) => {
+  res.send("Now you're the default")
+});
+
 app.listen(8081, () => {
-  console.log("Server is running on port on port 8081");
+  console.log("Server is running on port 8081 live");
 });
