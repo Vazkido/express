5031288d4d7bfd036d5ad2a9049d052783edd1ce
vazkido <vazkid02@gmail.com>
2025-03-17 09:15:10 -0400

    Morning Section

--- index.js
+++ index.js
@@ -4,22 +4,33 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
-app.get("/val", (req, res) => {
-  res.send("BE CAREFUL MY LOVE!");
-});
+// app.get("/val", (req, res) => {
+//   res.send("BE CAREFUL MY LOVE!");
+// });
 
-app.get("/val/:name", (req, res) => {
- res.send(`<h1> KIND MAN! ${req.params.name} </h1>`);
-});
+// app.get("/val/:name", (req, res) => {
+//  res.send(`<h1> KIND MAN! ${req.params.name} </h1>`);
+// });
 
-app.get("*", (req, res) => {
-  res.send("Now you're the default")
-});
+// app.get("*", (req, res) => {
+//   res.send("Now you're the default")
+// });
 
-app.listen(8081, () => {
-  console.log("Server is running on port 8081 live");
-});
+// app.listen(8081, () => {
+//   console.log("Server is running on port 8081 live");
+// });
+
+
+
+// This example was used in morning section guys express work!
+
+const users = [
+  {
+    id:2231,
+    name: "joseph"
+  },
+]
